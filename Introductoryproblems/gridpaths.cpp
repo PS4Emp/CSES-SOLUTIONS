d6e62a0b0b9120e032efa203371bd67405548c89
Ps4Atom <pranav2097@gmail.com>
2024-02-12 21:17:27 +0530

    error

---
+++ Introductoryproblems/gridpaths.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+typedef long long ll;
+
+const int mod = 1e9+7;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<string> grid(n);
+    for(int i = 0; i < n; i++)
+        cin >> grid[i];
+    vector< vector<ll> > numberOfPaths(n, vector<ll>(n, 0));
+    numberOfPaths[0][0] = (grid[0][0] == '.' ? 1 : 0);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(grid[i][j] != '*'){
+                if(i - 1 >= 0 && grid[i - 1][j] != '*')
+                    numberOfPaths[i][j] = (numberOfPaths[i][j] + numberOfPaths[i - 1][j]) % mod;
+                if(j - 1 >= 0 && grid[i][j - 1] != '*')
+                    numberOfPaths[i][j] = (numberOfPaths[i][j] + numberOfPaths[i][j - 1]) % mod;
+            }
+        }
+    }
+    cout << numberOfPaths[n - 1][n - 1] << endl; // Output the result
+    return 0;
+}
