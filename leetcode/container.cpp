e32a27a3775a8f594a3f8c6d1382386331fef90f
Ps4Atom <pranav2097@gmail.com>
2024-04-02 23:15:01 +0530

    --

---
+++ leetcode/container.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxArea(std::vector<int>& height) {
+        int maxArea = 0;
+        int left = 0, right = height.size() - 1;
+
+        while (left < right) {
+            
+            int currentArea = std::min(height[left], height[right]) * (right - left);
+            
+            maxArea = std::max(maxArea, currentArea);
+
+            
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+};
