518b4951376743bce366466ce0c9aa2971b89e15
Ps4Atom <pranav2097@gmail.com>
2024-04-11 23:03:18 +0530

    --

--- leetcode/integertoroman.cpp
+++ leetcode/integertoroman.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     std::string intToRoman(int num) {
+        
         int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         std::string symbols[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
@@ -8,9 +9,10 @@ public:
         int i = 0;
 
         while (num > 0) {
-            int count = num / values[i];
-            roman_numeral += std::string(count, symbols[i][0]);
-            num %= values[i];
+            while (num >= values[i]) {
+                roman_numeral += symbols[i];
+                num -= values[i];
+            }
             i++;
         }
 
